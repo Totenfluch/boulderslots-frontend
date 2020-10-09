49af0fcc041c969124f6f89f474fd69f24cc8ba1
Totenfluch <github@cziegler.de>
2020-10-09 16:39:07 +0200

    add build folder for easier deploys

---
+++ build/precache-manifest.0d398cb82d0aa9d451ef04eab5a0e78d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c60d8d557b860080424620ebbd1a9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27af0b1dd35c6985a9cc",
+    "url": "/static/js/2.7c0ecaf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7c0ecaf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0173bac7432cd1c7ba9e",
+    "url": "/static/js/main.35377951.chunk.js"
+  },
+  {
+    "revision": "efb106f9258dca924c51",
+    "url": "/static/js/runtime-main.9b3b44e0.js"
+  }
+]);
